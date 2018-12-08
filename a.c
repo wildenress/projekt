913d3a43cdfb83a259ec936417d39024a56c008a
wildenress <m.juhas.mj@gmail.com>
2018-12-08 07:11:21 -0800

    Main iba so strukturou
    Cisty projekt iba so strukturou

---
+++ a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct zaznam{
+  char meno_P[50];
+  char pohlavie[2];
+  int rok;
+  char spz[8];
+  int typ;
+  int vyska_p;
+  int datum;
+}ZAZNAM;
+
+
+int main(int argc, char const *argv[]) {
+  printf("Projekt pokuty.\n", );
+
+  return 0;
+}
